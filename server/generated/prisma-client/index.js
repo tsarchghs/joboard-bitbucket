555e726ab74e60802dc6d08784cda6f0033e6911
Gjergj Kadriu <gjergjk71@gmail.com>
2019-04-07 03:37:00 +0200

    Prisma client

---
+++ server/generated/prisma-client/index.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "File",
+    embedded: false
+  },
+  {
+    name: "Company",
+    embedded: false
+  },
+  {
+    name: "Job",
+    embedded: false
+  },
+  {
+    name: "JOB_TYPE",
+    embedded: false
+  },
+  {
+    name: "STATUS_TYPE",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/gjergj-kadriu-c6f550/joboard/dev`
+});
+exports.prisma = new exports.Prisma();
